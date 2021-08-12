e70037b5e2034c8c1da6f982b01f7685b76637c2
tomoki.mori <tomokey7@gmail.com>
2021-08-12 12:17:10 +0000

    change Thinkable module

--- main.rb
+++ main.rb
@@ -6,8 +6,8 @@ suzuki = Human.new("鈴木 次郎", 30, "野球")
 sato   = Human.new("佐藤 花子", 20, "漫画")
 
 tanaka.say
-tanaka.think(tanaka.hobby)
+tanaka.think
 suzuki.say
-suzuki.think(suzuki.hobby)
+suzuki.think
 sato.say
-sato.think(sato.hobby)
+sato.think
--- thinkable.rb
+++ thinkable.rb
@@ -1,5 +1,5 @@
 module Thinkable
-  def think(input)
-      puts "私は"+input+"について考えています。"
+  def think
+      puts "私は"+self.hobby+"について考えています。"
   end
 end
